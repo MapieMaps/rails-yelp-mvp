02b609cb639ecb15196f8c1bfe2cabc890f8920d
Marie-Pierre Jeffard <mariep.email@gmail.com>
2021-07-07 17:22:06 +0200

    Yelp MVP done, on verra pour le style une autre fois...

--- config/routes.rb
+++ config/routes.rb
@@ -1,32 +1,28 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'restaurants#index'
-  # A visitor can see the list of all restaurants.
-  # => GET "restaurants"
-  get "restaurants", to: "restaurants#index"
-
-  # A visitor can add a new restaurant,
-  # and be redirected to the show view of that new restaurant.
-  # => GET "restaurants/new"
-  get "restaurants/new", to: "restaurants#new"
-  # => POST "restaurants"
-  post "restaurants", to: "restaurants#create"
-
-  # A visitor can see the details of a restaurant,
-  # with all the reviews related to the restaurant.
-  # => GET "restaurants/38"
-  get "restaurants/:id", to: "restaurants#show", as: :restaurant
-
-  # resources :restaurants do
-  #   resources :reviews, only: [:new, :create]
-  #   # GET restaurants/42/reviews/new
-  # end
-
-  # resources :reviews, only: [:show]
 
   # A visitor can add a new review to a restaurant
   # => GET "restaurants/38/reviews/new" (nested resources)
   # => POST "restaurants/38/reviews"
 
   # A visitor CANNOT update / delete any restaurant or review.
+
+  resources :restaurants, only: [:index, :new, :create, :show] do
+    resources :reviews, only: [:new, :create]
+    # GET restaurants/42/reviews/new
+  end
+
+  resources :reviews, only: [:show]
 end
+
+
+#                Prefix Verb URI Pattern                                       Controller#Action
+#                  root GET  /                                                 restaurants#index
+#    restaurant_reviews POST /restaurants/:restaurant_id/reviews(.:format)     reviews#create
+# new_restaurant_review GET  /restaurants/:restaurant_id/reviews/new(.:format) reviews#new
+#           restaurants GET  /restaurants(.:format)                            restaurants#index
+#                       POST /restaurants(.:format)                            restaurants#create
+#        new_restaurant GET  /restaurants/new(.:format)                        restaurants#new
+#            restaurant GET  /restaurants/:id(.:format)                        restaurants#show
+#                review GET  /reviews/:id(.:format)                            reviews#show
